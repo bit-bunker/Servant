e553257a3c1442443d689aaac5408edd1af13d39
kalashnikov66p <kalashnikov66@proton.me>
2023-06-03 20:12:15 -0300

    using map inside memberIdsFromString function

--- src/utils/members.ts
+++ src/utils/members.ts
@@ -1,16 +1,11 @@
-import { Guild, Role } from 'discord.js';
-
 const userMentionRegExp = new RegExp('<@!*([0-9]+)>');
 const snowflakeRegExp = new RegExp('([0-9]+)');
 
 export function memberIdsFromString(members: string): string[] {
-    const memberIds: string[] = [];
-    members.split(' ').forEach((member: string) => {
+    return members.split(' ').map((member: string) => {
         const mentionMatch = userMentionRegExp.exec(member);
-        if (mentionMatch) memberIds.push(mentionMatch[1]);
+        if (mentionMatch) return mentionMatch[1];
         const snowflakeMatch = snowflakeRegExp.exec(member);
-        if (snowflakeMatch) memberIds.push(snowflakeMatch[1]);
+        if (snowflakeMatch) return snowflakeMatch[1];
     });
-
-    return memberIds;
 }
