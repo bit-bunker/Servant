cfb4d1d0941866f70507ab5f2016d9b2c4d63471
zDragonSK <skstudios.contato@gmail.com>
2023-06-02 22:55:06 -0300

    New commands: avatar, serverinfo and userinfo

---
+++ src/commands/geral/avatar.ts
@@ -0,0 +1,31 @@
+import { Client, Discord, Slash, SlashOption } from 'discordx';
+import { Category } from '@discordx/utilities';
+import { ApplicationCommandOptionType, CommandInteraction, User, Embed, EmbedBuilder } from 'discord.js';
+
+@Discord()
+@Category('info')
+export class avatar {
+  @Slash({
+    name: 'avatar',
+    description: "View a user's avatar."
+  })
+  async avatar(
+    @SlashOption({
+      description: "Member to display the avatar.",
+      name: "member",
+      type: ApplicationCommandOptionType.User,
+      required: true
+    })
+    member: User,
+    interaction: CommandInteraction,
+    client: Client) {
+    const user = interaction.guild?.members.cache.get(member.id);
+    const avatar = user?.user.avatarURL({ size: 1024 });
+    const embed = new EmbedBuilder()
+      .setTitle(`${user?.user.username}'s Avatar`)
+      .setImage(`${avatar}`)
+      .setColor("#2f3136")
+      .setTimestamp();
+    await interaction.reply({content: `${member}`, embeds: [embed]});
+  }
+}
---
+++ src/commands/geral/serverinfo.ts
@@ -0,0 +1,44 @@
+import { Client, Discord, Slash } from 'discordx';
+import { Category } from '@discordx/utilities';
+import { ApplicationCommandOptionType, CommandInteraction, EmbedBuilder } from 'discord.js';
+
+@Discord()
+@Category('serverinfo')
+export class serverinfo {
+  @Slash({
+    name: 'serverinfo',
+    description: "View server information."
+  })
+  async serverinfo(
+    interaction: CommandInteraction,
+    client: Client) {
+      const embed = new EmbedBuilder()
+        .setTitle(`${interaction.guild?.name}`)
+        .setThumbnail(`${interaction.guild?.iconURL()}`)
+        .setColor("#2f3136")
+        .setFields(
+          {
+            name: '<:moderator:957490077964066837> Owner',
+            value: `${interaction.guild?.members.cache.get(interaction.guild?.ownerId)}`,
+            inline: true
+          },
+          {
+            name: '<:Information:957490076567363645> ID',
+            value: `\`${interaction.guild?.id}\``,
+            inline: true
+          },
+          {
+            name: '<:UserRyze:742191096989351947> Members',
+            value: `\`${interaction.guild?.members.cache.size}\``,
+            inline: true
+          },
+          {
+            name: '<:Tag:742191097496862870> Roles',
+            value: `\`${interaction.guild?.roles.cache.size}\``,
+            inline: true
+          }
+        )
+        .setTimestamp();
+      interaction.reply({ embeds: [embed] });
+    }
+}
