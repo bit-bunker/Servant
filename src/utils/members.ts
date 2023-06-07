9d56870e7dbb370192862413f43378d0f2730039
Moizes J. Sousa <milyth@duck.com>
2023-06-07 13:16:44 -0300

    chore(utils/member): refactor code

--- src/utils/members.ts
+++ src/utils/members.ts
@@ -2,11 +2,7 @@ const userMentionRegExp = new RegExp('<@!*([0-9]+)>');
 const snowflakeRegExp = new RegExp('([0-9]+)');
 
 export function memberIdsFromString(members: string): string[] {
-    return members.split(' ').flatMap((member: string) => {
-        const mentionMatch = userMentionRegExp.exec(member);
-        if (mentionMatch) return [mentionMatch[1]];
-        const snowflakeMatch = snowflakeRegExp.exec(member);
-        if (snowflakeMatch) return [snowflakeMatch[1]];
-        return [];
-    });
+    return members.split(' ').flatMap((member: string) =>
+        userMentionRegExp.exec(member)?.at(1) ?? snowflakeRegExp.exec(member)?.at(1) ?? []
+    );
 }
