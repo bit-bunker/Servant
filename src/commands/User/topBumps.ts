429f8b853e473c467d7129f03e46aa1644c09d33
Moizes J. Sousa <milyth@duck.com>
2023-06-18 17:37:54 -0300

    Refactor /bumps and add /topbumps command

---
+++ src/commands/User/topBumps.ts
@@ -0,0 +1,41 @@
+import { Discord, Slash } from 'discordx';
+import { Category } from '@discordx/utilities';
+import { CommandInteraction, EmbedBuilder } from 'discord.js';
+import { container } from 'tsyringe';
+import { PrismaClient } from '@prisma/client';
+
+@Discord()
+@Category('User commands')
+export default abstract class TopBumpsCommand {
+    @Slash({
+        name: 'topbumps',
+        description: "Show server's top bumps"
+    })
+    async topBumps(interaction: CommandInteraction) {
+        const prisma: PrismaClient = container.resolve('PrismaClient');
+
+        const results = await prisma.users.findMany({
+            take: 6,
+            orderBy: {
+                bumpCount: 'desc'
+            }
+        });
+
+        if (!results.length) {
+            await interaction.reply({ ephemeral: true, content: 'Nobody bumped the server yet :c' });
+            return;
+        }
+
+        const formatted = results
+            .map((user, idx) => `- **${idx + 1}º ** <@${user.id}> bumped the server ${user.bumpCount} times!`)
+            .join('\n');
+
+        const embed = new EmbedBuilder()
+            .setDescription(formatted)
+            .setTitle('Top Bumps')
+            .setColor('Blurple')
+            .setTimestamp();
+
+        await interaction.reply({ embeds: [embed] });
+    }
+}
