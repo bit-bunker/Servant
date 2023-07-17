3d41ab3c731d67c9f945c050fd7fbfb098501045
zDragonSK <skstudios.contato@gmail.com>
2023-07-17 09:46:56 -0300

    [test] Update bump cron

---
+++ prisma/migrations/20230716183336_/migration.sql
@@ -0,0 +1,13 @@
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Users" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "bumpCount" INTEGER NOT NULL DEFAULT 0,
+    "balance" INTEGER NOT NULL DEFAULT 0,
+    "top" BOOLEAN NOT NULL DEFAULT false
+);
+INSERT INTO "new_Users" ("balance", "bumpCount", "id") SELECT "balance", "bumpCount", "id" FROM "Users";
+DROP TABLE "Users";
+ALTER TABLE "new_Users" RENAME TO "Users";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,12 +1,39 @@
 import { Client, Discord, Once, ArgsOf } from 'discordx';
 import { ActivityType } from 'discord.js';
+import {PrismaClient} from '@prisma/client';
+import { container } from 'tsyringe';
+import cron from 'node-cron';
 
 @Discord()
 export default class ReadyEvent {
     @Once({ event: 'ready' })
     async onReady([]: ArgsOf<'ready'>, client: Client) {
         await client.initApplicationCommands();
-        client.user?.setActivity({ name: `Life out`, type: ActivityType.Playing });
+
+        /*Bump Cron*/
+        const prisma: PrismaClient = container.resolve('PrismaClient');
+        cron.schedule('* * * * *', async () => {
+            const guild = await client.guilds.fetch("950325788429934652");
+            const role = await guild.roles.fetch("1129861625361485844");
+            if (role){
+                // remove old top ranking
+                const oldtop = await prisma.users.findFirst({where: {top: true}});
+                const o_member = await guild.members.fetch(oldtop!.id);
+                if (oldtop)
+                    o_member.roles.remove(role);
+                console.log(oldtop);
+                // add new top ranking
+                const top = await prisma.users.findFirst({orderBy: {bumpCount: 'desc'}});
+                const member = await guild.members.fetch(top!.id);
+                member.roles.add(role);
+                console.log(top);
+                // clean members
+                const users = await prisma.users.findMany();
+                users.map(async (user)=> prisma.users.update({where: {id: user.id}, data: {bumpCount:0, top: false}}));
+            }
+        });
+
+        client.user?.setActivity({ name: `Java out`, type: ActivityType.Playing });
         console.log(`${client.user?.tag} is ready!`);
     }
 }
